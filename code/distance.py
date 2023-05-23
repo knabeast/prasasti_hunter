b6792d3cb8d1fd8326fb342d0063108908f85e1a
Vito Febrian Ananta <vitobriannt@gmail.com>
2023-05-23 14:48:02 +0700

    feat: fix distance pos

--- code/distance.py
+++ code/distance.py
@@ -4,8 +4,9 @@ from settings import *
 class Distance:
     def __init__(self, surface):
         self.display_surface = surface
-        self.font = pygame.font.SysFont('minecraft', 30, bold=True)
-        self.distance_rect = pygame.Rect((screen_width/2 - 96, screen_height - 32), (30, 30))
+        # self.font = pygame.font.SysFont('minecraft', 30, bold=True)
+        self.font = pygame.font.Font('./graphics/font/minecraft.ttf', 24)
+        self.distance_rect = pygame.Rect((screen_width/2 - 108, screen_height - 33), (30, 30))
 
     def show_distance(self, num):
         distance_surf = self.font.render(f"Distance: {str(num)}", False, '#E6E6E6')
